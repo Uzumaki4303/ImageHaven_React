9ec3623896475a2a0a0c7b0065afd1d6179fbc43
Uzumaki4303 <amitnare4303@gmail.com>
2024-09-20 22:07:49 +0530

    update : vite.config and package.json for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ImageHaven_React/',  // Add this line to specify the base path for GitHub Pages
   plugins: [react()],
 })
