4200c55f46e9d586cea090c74281dcc1ce5a7d05
Uzumaki4303 <amitnare4303@gmail.com>
2024-09-20 22:24:40 +0530

    Revert "update : vite.config and package.json for gh-pages"
    This reverts commit 9ec3623896475a2a0a0c7b0065afd1d6179fbc43.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ImageHaven_React/',  // Add this line to specify the base path for GitHub Pages
   plugins: [react()],
 })
